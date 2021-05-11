40eadbdf6126ec4a82019165ff0395e540d7a4d7
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-05-11 16:33:51 -0500

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { animals } = require('./data/animals');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -41,12 +42,29 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id) [0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    res.json(result);
+});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
